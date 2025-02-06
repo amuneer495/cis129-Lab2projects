c86547aeb1d2500c1b2dd504aab3ae3b5e4b79ae
amuneer495 <amuneer@mail.pima.edu>
2025-02-06 12:35:37 -0700

    Update cis129_lab02_shortStory.py
    My Class is CIS 129

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello, World!') 
+# Author: Student McLearner
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print("Hello world")
+print("I like My cvlass")
+print("My Class in Downtown")
